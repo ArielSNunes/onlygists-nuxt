93c6e5ca7ae3b22f76543cc60967984d67854210
Ariel dos Santos Nunes <arielsn1@gmail.com>
2024-03-12 10:32:44 -0300

    feat: add main content and some components

---
+++ components/Logo/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="items-center gap-2 select-none">
+        <span class="text-2xl">🤩</span>
+        <span class="font-[Inter] font-bold text-gray-700 text-lg md:text-xl">
+            onlygists
+        </span>
+    </div>
+</template>
---
+++ components/MainContent/MainContent.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full h-full flex flex-col">
+        <slot name="header" class="flex-1" />
+        <div class="bg-gray-50 h-max w-full flex-1">
+            <div class="w-full h-full p-5 mx-auto max-w-screen-xl">
+                <slot name="content" />
+            </div>
+        </div>
+    </div>
+</template>
